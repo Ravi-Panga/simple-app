df1cd4f85d936888edfb6e75217d8584a07408de
ravi.panga@oracle.com <ravi.panga@oracle.com>
2022-01-27 15:45:40 +0530

    added webapp

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "src",
+    version = "0.0.1",
+    description = "It's Wine 0 package",
+    author = "Ravi Panga",
+    packages = find_packages(),
+    license = "MIT"
+)
