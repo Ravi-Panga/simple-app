ef2ac92700253f400f68119cb271b19049bb21c2
ravi.panga@oracle.com <ravi.panga@oracle.com>
2022-01-28 11:53:26 +0530

    git cicd pipeline added

--- app.py
+++ app.py
@@ -8,27 +8,14 @@ import numpy as np
 import yaml
 import joblib
 import json
+from prediction_service import prediction
 
 webapp_root = "webapp"
-params_path = "params.yaml"
 
 static_dir = os.path.join(webapp_root, "static")
 template_dir = os.path.join(webapp_root, "templates")
 
 app = Flask(__name__, static_folder=static_dir, template_folder=template_dir)
-
-def read_params(config_path) :
-    with open(config_path) as yaml_file:
-        config = yaml.safe_load(yaml_file)
-    return config
-
-def predict(data):
-    config = read_params(params_path)
-    model_dir_path = config["webapp_model_dir"]
-    model = joblib.load(model_dir_path)
-    prediction = model.predict(data)
-    print(prediction)
-    return prediction
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -36,12 +23,13 @@ def index():
     if request.method == "POST":
         try:
             if request.form :
-                data = dict(request.form).values()
-                data = [list(map(float, data))]
-                response = predict(data)
+                dict_req = dict(request.form)
+                response = prediction.form_response(dict_req)
                 return render_template("index.html", response = response)
             elif request.json:
-                pass
+                response = prediction.api_response(request.json)
+                return jsonify(response)
+
         except Exception as e:
             print(e)
             error = {"error": "Something went wrong!! try again"}
